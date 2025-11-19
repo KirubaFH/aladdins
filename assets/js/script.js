986af801d26f1741cc08f84d326dad5f859c818e
KirubaFH <kirubakaran.k@foodhub.com>
2025-11-20 02:25:43 +0530

    Event page articles section

--- assets/js/script.js
+++ assets/js/script.js
@@ -384,3 +384,38 @@ $('.awards-slides').owlCarousel({
     }
 });
 
+$('.samples-slider').owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: true,
+    dots: false,
+    navText: ["<i class='fa fa-caret-left'></i>", "<i class='fa fa-caret-right'></i>"],
+    smartSpeed: 1000,
+    autoplay: false,
+    autoplayTimeout: 4000,
+    smartSpeed: 1000,
+    responsive: {
+        0: {
+            items: 1
+        },
+        576: {
+            items: 2
+        },
+        768: {
+            items: 3
+        },
+        1200: {
+            items: 4
+        }
+    }
+})
+
+lc_lightbox('.lightbox', {
+    wrap_class: 'lcl_fade_oc',
+    gallery: false,
+    thumb_attr: 'data-lcl-thumb',
+    skin: 'light',
+    radius: 4,
+    padding: 0,
+    border_w: 0
+});
